a0935d932077196b8cca9757ba0ae90667b2dc18
Ivan Čabraja <ivancabraja0@gmail.com>
2023-12-22 17:49:46 +0100

    promijenjen cijeli layout od aplikacije

--- app/src/main/java/com/example/projektma/MainActivity.java
+++ app/src/main/java/com/example/projektma/MainActivity.java
@@ -12,35 +12,12 @@ import com.example.projektma.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
 
-    ActivityMainBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        ReplaceFragment(new TaskList());
-        binding.bottomNavigationView.setOnItemSelectedListener(item ->{
 
-            int itemId = item.getItemId();
-
-            if (itemId == R.id.TaskList) {
-                ReplaceFragment(new TaskList());
-            } else if (itemId == R.id.AddTask) {
-                ReplaceFragment(new AddTask());
-            } else if (itemId == R.id.Completed) {
-                ReplaceFragment(new Completed_tasks());
-            }
-
-            return true;
-        });
     }
-    private void ReplaceFragment (Fragment fragLayout){
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frame_layout, fragLayout);
-        fragmentTransaction.commit();
-    }
+
 
 }
